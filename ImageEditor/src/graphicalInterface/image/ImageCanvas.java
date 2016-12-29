0a531577196af4e8c959c80bb0e5047f1a02d0fa
Paul <p.landymore@gmail.com>
2016-12-29 20:59:20 +0000

    Change cursor when mouse hovers over image area

--- ImageEditor/src/graphicalInterface/image/ImageCanvas.java
+++ ImageEditor/src/graphicalInterface/image/ImageCanvas.java
@@ -1,5 +1,6 @@
 package graphicalInterface.image;
 
+import javafx.scene.Cursor;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
@@ -10,5 +11,11 @@ public class ImageCanvas extends Canvas {
 		
 		GraphicsContext gc = this.getGraphicsContext2D();
 		gc.drawImage( p_image, 0, 0 );
+		
+		setDefaultCursor();
+	}
+	
+	private void setDefaultCursor() {
+		super.setCursor( Cursor.CROSSHAIR );
 	}
 }
